b978641526c7d733f2915820d1189b95dd3b14fe
Dmitriy <kerokero@yandex.ru>
2024-04-25 13:34:38 +0000

    fix tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,7 +2,7 @@ from hexlet_pytest.example import reverse
 
 
 def test_reverse():
-    assert reverse('Hexlet') == 'telxeH1'
+    assert reverse('Hexlet') == 'telxeH'
 
 
 def test_reverse_for_empty_string():
